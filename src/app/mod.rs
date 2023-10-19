a05394f9cc7be46408d048ad43e1a52a33423dd1
Dofe <thibo1503@gmail.com>
2023-10-19 14:43:36 +0200

    Edit `MyApp` members

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -4,11 +4,15 @@ use editor::{
     room::RoomEditor
 };
 
+use crate::worker::model::structs::{
+    DRODataManager
+};
+
 use egui::*;
 
 pub struct MyApp {
-    name: String,
-    age: u32,
+    dro_struct: DRODataManager,
+
     editor: Vec<Box<dyn Editor>>,
     selected_editor: usize,
 }
@@ -16,10 +20,10 @@ pub struct MyApp {
 impl Default for MyApp {
     fn default() -> Self {
         Self {
-            name: "Arthur".to_owned(),
-            age: 42,
+            dro_struct: DRODataManager::new(),
+
             editor: vec![Box::new(RoomEditor::new())],
-            selected_editor: 0
+            selected_editor: 0            
         }
     }
 }
@@ -68,24 +72,24 @@ impl MyApp {
         })});
     }
 
-
-    fn hello_world(&mut self, ui: &mut Ui) {
-        ui.vertical(|ui| {
-            ui.heading("My egui Application");
-            ui.horizontal(|ui| {
-                let name_label = ui.label("Your name: ");
-                ui.text_edit_singleline(&mut self.name)
-                    .labelled_by(name_label.id);
-            });
-            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-            if ui.button("Click each year").clicked() {
-                self.age += 1;
-            }
-            ui.label(format!("Hello '{}', age {}", self.name, self.age));
-    
-            ui.image(egui::include_image!(
-                "../../assets/edit_Picasso.png"
-            ));
-        });
-    }
+    // TODO: Remove code sample
+    //fn hello_world(&mut self, ui: &mut Ui) {
+    //    ui.vertical(|ui| {
+    //        ui.heading("My egui Application");
+    //        ui.horizontal(|ui| {
+    //            let name_label = ui.label("Your name: ");
+    //            ui.text_edit_singleline(&mut self.name)
+    //                .labelled_by(name_label.id);
+    //        });
+    //        ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+    //        if ui.button("Click each year").clicked() {
+    //            self.age += 1;
+    //        }
+    //        ui.label(format!("Hello '{}', age {}", self.name, self.age));
+    //
+    //        ui.image(egui::include_image!(
+    //            "../../assets/edit_Picasso.png"
+    //        ));
+    //    });
+    //}
 }
